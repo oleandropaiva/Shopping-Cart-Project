9b48434a0d4aa6107bff511680f2600cc4f89919
Leandro Paiva <leandroapaiva@yahoo.com>
2022-02-14 13:32:53 -0300

    item 9 e 2

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,24 +3,24 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fecthItem', () => {
-  it('Verifica se fetchProducts é uma função', () =>  {
-    expect(typeof fetchProducts).toBe('function');
+  it('Verifica se fetchItem é uma função', () =>  {
+    expect(typeof fetchItem).toBe('function');
   });
   it('Verifica se fetch foi chamado', async () => {
-   await fetchProducts('MLB1615760527');
+   await fetchItem('MLB1615760527');
    expect(fetch).toHaveBeenCalled()
   });
   it('Verifica se fetch foi chamado com endpoint correto', async () => {
-    await fetchProducts('computador');
-    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527')
    });
    it('Testa se o objeto é igual ao esperado', async () => {
-    const computador = await fetchProducts('computador');
+    const computador = await fetchItem('MLB1615760527');
     expect(computador).toMatchObject(item)
 });
-it('Testa se ao chamar o fetchProducts sem argumento retorna um erro', async () => {
-  const fetchSemArgumento = await fetchProducts();
-  const erro = new Error('You must provide an url');
-  expect(fetchSemArgumento).toEqual(erro);  
-});
+  it('Testa se ao chamar o fetchProducts sem argumento retorna um erro', async () => {
+    const fetchSemArgumento = await fetchItem();
+    const erro = new Error('You must provide an url');
+    expect(fetchSemArgumento).toEqual(erro);
+})
 });

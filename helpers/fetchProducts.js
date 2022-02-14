12029203cff92c40bb6fc08baf531e8f05fca4b9
Leandro Paiva <leandroapaiva@yahoo.com>
2022-02-14 11:09:54 -0300

    fecthProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,7 +1,14 @@
-const fetchProducts = () => {
-  // seu código aqui
+const fetchProducts = async (item) => {
+try { 
+    const mercadoLivre = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`
+    const response = await fetch (mercadoLivre);
+    const object = await response.json();
+    return object.results;
+  } catch (error) {
+    return new Error('You must provide an url');
+}
 };
-
+console.log(fetchProducts('computador'))
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,11 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Verifica se fetchProducts é uma função', () =>  {
+    expect(typeof fetchProducts).toBe('function');
+  })
+  it('Verifica se o retorno é igual ao esperado', async () => {
+   await fetchProducts('computador');
+   expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  })
 });
